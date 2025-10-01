cebb237d197cf2b279e0e20b51c6de8c6e5ee3d5
AlHamid-dev <alhamid.pro.dev@gmail.com>
2025-10-02 00:12:17 +0530

    added initial canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
+import { useState } from 'react'
 import './App.css'
+import FlowCanvas from './components/FlowCanvas'
 
 function App() {
-
+  const [isMiniMap, setIsMiniMap] = useState(false)
+  const toggleMiniMap = () => {
+    setIsMiniMap(!isMiniMap)
+  }
+  
   return (
     <>
-      <div>
-       hello world
+      <div className='container-fluid'>
+        <div className="d-flex justify-content-start g-2">
+          <button className='btn btn-primary' onClick={toggleMiniMap}>
+            {!isMiniMap ? "Enable Mini Map" : "Disable MiniMap"}
+          </button>
+          <button className='btn btn-primary'>Clear</button>
+        </div>
+        <div className='my-2' style={{ border: "1px solid black", width: "90vw", height: "90vh", overflow: "hidden" }}>
+          <FlowCanvas isMiniMap={isMiniMap} />
+        </div>
       </div>
-      
     </>
   )
 }
---
+++ src/components/FlowCanvas.jsx
@@ -0,0 +1,64 @@
+import { useState, useCallback } from 'react';
+import { ReactFlow, Background, Controls, applyEdgeChanges, applyNodeChanges, addEdge, MiniMap } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
+const initialNodes = [
+  {
+    id: 'n1',
+    position: { x: 0, y: 0 },
+    data: { label: 'Node 1' },
+    type: 'input',
+  },
+  {
+    id: 'n2',
+    position: { x: 100, y: 100 },
+    data: { label: 'Node 2' },
+  },
+]
+
+const initialEdges = [
+  { id: 'n1-n2', source: 'n1', target: 'n2', label: 'connects with', type: 'step' },
+]
+
+const FlowCanvas = ({ isMiniMap }) => {
+  const proOptions = { hideAttribution: true };
+
+  const [nodes, setNodes] = useState(initialNodes)
+  const [edges, setEdges] = useState(initialEdges)
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  )
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  )
+
+  const onConnect = useCallback(
+    (connection) => {
+      setEdges((oldEdges) => addEdge(connection, oldEdges));
+    },
+    [setEdges],
+  )
+
+  return (
+    <div style={{ height: "100%" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        proOptions={proOptions}
+        fitView
+      >
+        {isMiniMap && <MiniMap zoomable pannable />}
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  )
+}
+
+export default FlowCanvas
